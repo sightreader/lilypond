52ae92ea4979ae06db02ae9dae9d96e11eb51026
David Kastrup <dak@gnu.org>
2013-07-17 10:35:35 +0200

    x

--- scm/define-grobs.scm
+++ scm/define-grobs.scm
@@ -2343,7 +2343,7 @@
 	(stencil . ,ly:text-interface::print)
 	(vertical-skylines . ,grob::always-vertical-skylines-from-stencil)
 	;; todo: add X self alignment?
-	(Y-extent . ,grob::all-heights-from-stencil)
+	(Y-extent . ,grob::always-Y-extent-from-stencil)
 	(X-offset . ,ly:self-alignment-interface::x-aligned-on-self)
 	(Y-offset . ,ly:side-position-interface::y-aligned-side)
 	(meta . ((class . Item)
