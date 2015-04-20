f6b0487637768b3561afecae27fa76512681e0d8
Abraham Lee <tisimst.lilypond@gmail.com>
2015-04-20 18:18:32 +0200

    Add add-music-font function to load individual fonts
    This commit allows to add single fonts to an empty font-tree.
    This makes it possible to load the notation fonts independently from the
    text fonts.

--- scm/font.scm
+++ scm/font.scm
@@ -148,7 +148,7 @@
 ;; Each size family is a vector of fonts, loaded with a delay.  The
 ;; vector should be sorted according to ascending design size.
 (define-public (add-music-fonts node family name brace design-size-alist factor)
-  "Set up music fonts.
+  "Set up music a set of optical music fonts.
 
 Arguments:
 @itemize
@@ -205,6 +205,62 @@ used.  This is used to select the proper design size for the text fonts.
                             (format #f "~a-brace" brace)))))
      )))
 
+;; *****************************************************************************
+;; custom function for loading a single-size music font
+;; *****************************************************************************
+(define-public (add-music-font node family name brace factor)
+  "Set up a music font without optical sizes.
+
+Arguments:
+@itemize
+@item
+@var{node} is the font tree to modify.
+
+@item
+@var{family} is the family name of the music font.
+
+@item
+@var{name} is the name for the music font.
+@file{@var{name}.otf} should be the music font,
+
+@item
+@var{brace} is the name for the brace font.
+@file{@var{brace}.otf} should have piano braces.
+
+@item
+@var{factor} is a size factor relative to the default size that is being
+used.  This is used to select the proper design size for the text fonts.
+@end itemize"
+  ;; These are dummy numbers that don't actually get used. I just didn't know
+  ;; how to get rid of at least one pair without breaking the code
+  (define design-size-alist '((20 . 20)))
+  (for-each
+   (lambda (x)
+     (add-font node
+               (list (cons 'font-encoding (car x))
+                     (cons 'font-family family))
+               (cons (* factor (cadr x))
+                     (caddr x))))
+
+   `((fetaText ,(ly:pt 20.0)
+               ,(list->vector
+                 (map (lambda (tup)
+                        (cons (ly:pt (cdr tup))
+                              (format #f "~a" name)))
+                      design-size-alist)))
+     (fetaMusic ,(ly:pt 20.0)
+                ,(list->vector
+                  (map (lambda (size-tup)
+                         (delay (ly:system-font-load
+                                 (format #f "~a" name))))
+                       design-size-alist
+                       )))
+     (fetaBraces ,(ly:pt 20.0)
+                 #(,(delay (ly:system-font-load
+                            (format #f "~a" brace)))))
+     )))
+;; *****************************************************************************
+
 (define-public (add-pango-fonts node lily-family family factor)
   ;; Synchronized with the `text-font-size' variable in
   ;; layout-set-absolute-staff-size-in-module (see paper.scm).
@@ -245,11 +301,11 @@ used.  This is used to select the proper design size for the text fonts.
 ;       ))
 ;   }
 ;
-; Leaving out "#:brace", "#:sans", and "#:typewriter" leave them at 
+; Leaving out "#:brace", "#:sans", and "#:typewriter" leave them at
 ; "emmentaler", "sans-serif", and "monospace", respectively. All fonts are
 ; still accesible through the usual scheme symbols: 'feta, 'roman, 'sans, and
 ; 'typewriter.
-(define*-public (set-global-fonts #:key 
+(define*-public (set-global-fonts #:key
   (music "emmentaler")
   (brace "emmentaler")
   (roman "Century Schoolbook L")
@@ -262,7 +318,13 @@ used.  This is used to select the proper design size for the text fonts.
     (add-pango-fonts n 'sans sans factor)
     (add-pango-fonts n 'typewriter typewriter factor)
     n))
-    
+
+;; *****************************************************************************
+(define*-public (create-empty-font-tree)
+  (let ((n (make-font-tree-node 'font-encoding 'fetaMusic)))
+    n))
+;; *****************************************************************************
+
 (define-public (make-pango-font-tree roman-str sans-str typewrite-str factor)
   (let ((n (make-font-tree-node 'font-encoding 'fetaMusic)))
     (add-music-fonts n 'feta "emmentaler" "emmentaler" feta-design-size-mapping factor)
