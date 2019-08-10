79c7138426d1d7d4bf911ee406e47fe701487885
Knut Petersen <knut_petersen@t-online.de>
2019-08-10 08:28:06 +0200

    "#!/usr/bin/python" => "#!/usr/bin/python3" in some python scripts

--- python/auxiliar/manuals_definitions.py
+++ python/auxiliar/manuals_definitions.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # This module is imported by check_texi_refs.py
 
--- scripts/build/pytt.py
+++ scripts/build/pytt.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
 '''
     Copyright (C) 2008--2019 Jan Nieuwenhuizen <janneke@gnu.org>
